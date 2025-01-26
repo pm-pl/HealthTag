3670f0fb9be323c782e5fc63deee2c1cf48bbbf1
KanekiLeChomeur <52374013+KanekiLeChomeur@users.noreply.github.com>
2025-01-26 01:25:17 +0100

    Update Main.php
    Performance fix:
    - Less Config calls

--- src/shelly7w7/HealthTag/Main.php
+++ src/shelly7w7/HealthTag/Main.php
@@ -12,6 +12,8 @@ class Main extends PluginBase {
 
 	use SingletonTrait;
 
+	public array $configdata = [];
+
 	public function onEnable(): void {
 		self::setInstance($this);
 		$this->getServer()->getCommandMap()->register("healthtag", new HealthTagCommand($this));
@@ -19,12 +21,18 @@ class Main extends PluginBase {
 
 		$config = $this->getConfig();
 		$config->reload();
+		$this->configdata = $this->getConfig()->getAll();
+	}
+
+	public function onDisabel(): void {
+		$this->getConfig()->setAll($this->configdata);
+		$this->getConfig()->save();
 	}
 
 	public function updateScoreTag(Player $player): void{
-		if($this->getConfig()->get("type") === "custom") {
-			$player->setScoreTag(str_replace(["{health}", "{maxhealth}"], [$player->getHealth(), $player->getMaxHealth()], Main::getInstance()->getConfig()->getNested("customformat")));
-		} else if(Main::getInstance()->getConfig()->get("type") === "bar") {
+		if($this->configdata["type"] === "custom") {
+			$player->setScoreTag(str_replace(["{health}", "{maxhealth}"], [$player->getHealth(), $player->getMaxHealth()], $this->configdata["customformat"]));
+		} else if($this->configdata["type"] === "bar") {
 			$player->setScoreTag(str_repeat("§a|", (int)round($player->getHealth(), 0)) . str_repeat("§c|", (int)round($player->getMaxHealth() - $player->getHealth(), 0)));
 		} else {
 			$player->setScoreTag("Invalid type chosen for healthtag");
